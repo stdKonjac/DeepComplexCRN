c9e71efd6377c492938a3c26b9d8093bb9efbfc5
stdKonjac <918836143@qq.com>
2021-03-21 02:09:51 +0800

    first commit

---
+++ debug.py
@@ -0,0 +1,21 @@
+import torch
+from DCCRN import dccrn
+
+if __name__ == '__main__':
+    torch.manual_seed(10)
+    torch.autograd.set_detect_anomaly(True)
+    inputs = torch.randn([10, 16000 * 4]).clamp_(-1, 1)
+    labels = torch.randn([10, 16000 * 4]).clamp_(-1, 1)
+
+    # DCCRN-E
+    # model = dccrn('E')
+    # DCCRN-R
+    # model = dccrn('R')
+    # DCCRN-C
+    # model = dccrn('C')
+    # DCCRN-CL
+    model = dccrn('CL')
+
+    outputs = model(inputs)[1]
+    loss = model.loss(outputs, labels, loss_mode='SI-SNR')
+    print(loss)
